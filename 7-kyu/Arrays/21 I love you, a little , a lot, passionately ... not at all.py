05e384b431514a006c7af28c4d4dde67eea040ad
roots <akramovabdullo2002@gmail.com>
2024-02-11 15:32:45 +0500

    7 kyu, I love you, a little , a lot, passionately not at all

--- 7-kyu/Arrays/21 I love you, a little , a lot, passionately ... not at all.py
+++ 7-kyu/Arrays/21 I love you, a little , a lot, passionately ... not at all.py
@@ -28,3 +28,8 @@ def how_much_i_love_you(nb_petals):
 
 print(how_much_i_love_you(5))  # Output: "I love you"
 
+
+# method codewars
+
+def how_much_i_love_you(nb_petals):
+    return ["I love you", "a little", "a lot", "passionately", "madly", "not at all"][nb_petals % 6 - 1]
